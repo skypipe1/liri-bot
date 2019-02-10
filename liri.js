e28dbaca4c0aa0ec91d390480f5585d323eac160
Piper <skylarpiper710@gmail.com>
2019-02-10 15:27:18 -0600

    omdb working

--- liri.js
+++ liri.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var request = require("request");
 
-var callSpotify = function(songName){
+var callSpotify = function (songName) {
 
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
         var songs = data.tracks.items;
         for (var i = 0; i < songs.length; i++) {
@@ -25,19 +26,55 @@ var callSpotify = function(songName){
     });
 };
 
+var callOmdb = function (movieName) {
 
-var sCase = function(caseData, functionData){
-    switch(caseData){
+    var urlHit = "http://www.omdbapi.com/?apikey=4359dc59&t=" + movieName + "&tomatoes=true&y=&plot=short&r=json";
+    request(urlHit, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var jsonData = JSON.parse(body);
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+        };
+    });
+};
+var callBand = function (artist) {
+
+    var urlHit = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    request(urlHit, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var jsonData = JSON.parse(body);
+            console.log(data);
+
+        };
+    });
+};
+
+
+var sCase = function (caseData, functionData) {
+    switch (caseData) {
         case 'spotify-this-song':
             callSpotify(functionData);
             break;
+        case 'movie-this':
+            callOmdb(functionData);
+            break;
+        case 'concert-this':
+            callBand(functionData);
+            break;
         default:
-        console.log("LIRI does not know that")
-    }
-}
+            console.log("LIRI does not understand");
+    };
+};
 
-var run = function(argOne, argTwo){
-    sCase(argOne,argTwo)
+var run = function (argOne, argTwo) {
+    sCase(argOne, argTwo)
 };
 
 run(process.argv[2], process.argv[3]);
